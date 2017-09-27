5c14baa2bad16c87f778962f662370920f146c04
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-27 11:45:53 +0200

    day 1 - objects - ex. 1.1

---
+++ 1_Zadania/Dzien_1/3_Obiekty/js/app.js
@@ -0,0 +1,20 @@
+console.log("--------------------> Z a d a n i e 1 z wykl <---------------");
+
+// String.prototype.upperLower = function () {
+//
+//
+// };
+
+console.log("--------------------> Z a d a n i e 1 <---------------");
+
+var book = {
+    title: "Godfather",
+    author: "Mario Puzu",
+    numberOfPages: 500,
+}
+
+console.log(book.title);
+console.log(book.author);
+console.log(book.numberOfPages);
+
+console.log("--------------------> Z a d a n i e 2 <---------------");
