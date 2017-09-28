e6c92dea8530dba9a1c61bc908c22123aa4203db
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-28 15:38:33 +0200

    day 1 - advanced function - ex. 1.3

--- 1_Zadania/Dzien_1/4_Zaawansowane_funkcje/js/app.js
+++ 1_Zadania/Dzien_1/4_Zaawansowane_funkcje/js/app.js
@@ -1,13 +1,27 @@
 console.log("--------------------> Z a d a n i e 2  <---------------");
 
-function maxNumber() {
-    var max = 1;
-    for (var i = 0; i < arguments.length; i++) {
-        if (max < arguments[i]) {
-            max = arguments[i];
+    function maxNumber() {
+        var max = 1;
+        for (var i = 0; i < arguments.length; i++) {
+            if (max < arguments[i]) {
+                max = arguments[i];
+            }
         }
+        return max;
     }
-    return max;
-}
 
-console.log(maxNumber(3,45, 23, 56, 1123, 76, 45, 3));
+    console.log(maxNumber(3,45, 23, 56, 1123, 76, 45, 3));
+
+console.log("--------------------> Z a d a n i e 3  <---------------");
+
+    function countHello(number) {
+        var counter = 0;
+        var licznik = setInterval(function()	{
+                counter += 1
+        		console.log('Hello '+(counter));
+                if (counter == number) {
+                    clearInterval(licznik);
+                }
+        },	1000);
+    }
+    countHello(4);
