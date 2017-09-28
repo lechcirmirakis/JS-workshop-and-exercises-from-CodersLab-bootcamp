54f47c110d97914c35fd643b7c837d6868f53053
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-28 16:17:37 +0200

    day 2 - DOM searching - ex. 1.1

---
+++ 1_Zadania/Dzien_2/1_DOM_Wyszukiwanie_elementow/js/app.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+console.log("--------------------> Z a d a n i e 1  <---------------");
+
+    // var home = document.getElementById('home');
+    var home = document.querySelector('#home');
+    console.log(home);
+
+    var li01 = document.querySelector("li:not([data-direction])");
+    console.log(li01);
+
+    var block01 = document.querySelector(".block");
+    console.log(block01);
+
+console.log("--------------------> Z a d a n i e 2  <---------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
