d254443b9c127fe2eb2c2cb97781f41b18d4dc57
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-28 18:08:35 +0200

    day 2 - DOM element - ex. 1.1

--- 1_Zadania/Dzien_2/2_DOM_Element/js/app.js
+++ 1_Zadania/Dzien_2/2_DOM_Element/js/app.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    var homeElement = document.getElementById("home");
+    var childElements = document.querySelector(".oferts").children;
+    var banner = document.querySelector(".ban");
+    var blocks = document.querySelectorAll(".block");
+    var links = document.querySelector(".links").children;
+
 console.log("--------------------> Z a d a n i e 1 z wykl  <---------------");
 
-    var links = document.querySelectorAll('li');
     console.log(links);
 
     function getDatasInfo(elements) {
@@ -13,5 +18,34 @@ console.log("--------------------> Z a d a n i e 1 z wykl  <---------------");
         return linksData
     };
     console.log(getDatasInfo(links));
+
+    console.log("--------------------> Z a d a n i e 1  <---------------");
+
+    console.log(homeElement);
+
+    console.log(childElements); // tablica
+    for (var i = 0; i < childElements.length; i++) {
+        console.log(">>>>> "+childElements[i].tagName);
+        console.log(">>>>> "+childElements[i].className);
+    }
+    console.log(banner);
+
+    console.log(blocks); // tablica
+    for (var i = 0; i < blocks.length; i++) {
+        console.log(">>>>> "+blocks[i].tagName);
+        console.log(">>>>> "+blocks[i].className);
+    }
+
+    console.log(links); // tablica
+    for (var i = 0; i < links.length; i++) {
+        console.log(">>>>> "+links[i].tagName);
+        console.log(">>>>> "+links[i].className);
+    }
+
+    console.log("--------------------> Z a d a n i e 2  <---------------");
+
+
+
+
 
 });
