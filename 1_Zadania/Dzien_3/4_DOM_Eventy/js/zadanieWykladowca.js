7741cb77a1e416a74e948adb31b9f2e31acd87e6
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-29 16:33:29 +0200

    day 3 - DOM events - ex. 1.0

---
+++ 1_Zadania/Dzien_3/4_DOM_Eventy/js/zadanieWykladowca.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var parent = document.querySelectorAll('.parent');
+    var children = document.querySelectorAll('.children');
+
+    function showChildren(event) {
+        var child = this.querySelector('.children');
+        child.style.display ='block';
+    }
+
+    for (var i = 0; i < parent.length; i++) {
+        parent[i].addEventListener('mouseover', showChildren);
+    }
+
+    // nie skończone
+
+
+
+});
