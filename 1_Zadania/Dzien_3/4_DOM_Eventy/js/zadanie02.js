01c97f08226c800193c770ce1818d8415118d67f
GruzinHW <lech.cirmirakis@go2.pl>
2017-09-30 13:21:14 +0200

    day 3 - DOM events - ex. 1.2

---
+++ 1_Zadania/Dzien_3/4_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var button = document.getElementById('mainBtn');
+
+    button.addEventListener('click', function(event) {
+        console.log("Hurra działa");
+    });
+
+});
