4d521a26b025c5f9e6ca21f14edb7cbca8ef6e6c
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-02 14:51:56 +0200

    day 4 - DOM create elements - ex. 1.5

--- 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
+++ 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanie05.js
@@ -1,3 +1,34 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    //  nie skończone
+
+    function changeSide() {
+        var list01 = document.querySelector('#list1');
+        var list02 = document.querySelector('#list2');
+        var elementsOfList01 = list01.querySelectorAll('li');
+        var elementsOfList02 = list02.querySelectorAll('li');
+        for (var i = 0; i < elementsOfList01.length; i++) {
+            elementsOfList01[i].querySelector('a').addEventListener('click', function() {
+                var toClone = this.parentElement;
+                var newLi = toClone.cloneNode(true);
+                list02.appendChild(newLi);
+                toClone.parentElement.removeChild(toClone);
+
+            });
+
+        }
+        for (var i = 0; i < elementsOfList02.length; i++) {
+            elementsOfList02[i].querySelector('a').addEventListener('click', function() {
+                var toClone = this.parentElement;
+                var newLi = toClone.cloneNode(true);
+                list01.appendChild(newLi);
+                toClone.parentElement.removeChild(toClone);
+
+            });
+
+        }
+    }
+
+    changeSide();
+
+});
