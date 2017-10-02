fbb681dd76d6fcdf9a7e8299ab94e050c0d7db57
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-02 17:33:03 +0200

    day 4 - DOM create elements - ex. 1.0

--- 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanieWykladowca.js
+++ 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanieWykladowca.js
@@ -1,3 +1,12 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var buttons = document.querySelectorAll('a');
+
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            var tr = this.parentElement.parentElement;
+            tr.parentElement.removeChild(tr);
+        });
+    }
+
+});
