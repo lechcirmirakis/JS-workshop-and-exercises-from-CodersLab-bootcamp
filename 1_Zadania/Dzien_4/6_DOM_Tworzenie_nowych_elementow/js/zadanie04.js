0f39e42d5de215fa3e123fee7b2065ad321e76c0
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-02 14:17:09 +0200

    day 4 - DOM create elements - ex. 1.4

--- 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanie04.js
+++ 1_Zadania/Dzien_4/6_DOM_Tworzenie_nowych_elementow/js/zadanie04.js
@@ -1,3 +1,14 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', function () {
+
+    var button = document.querySelector('a');
+    var list = document.querySelectorAll('li')
+    console.log(list);
+
+    button.addEventListener('click', function() {
+        for (var i = 0; i < list.length; i++) {
+            list[i].parentElement.removeChild(list[i]);
+        }
+
+    });
+
+});
