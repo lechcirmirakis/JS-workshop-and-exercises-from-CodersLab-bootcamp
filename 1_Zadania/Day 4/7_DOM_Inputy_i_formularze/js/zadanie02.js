06a000aca708f913d7cac983b53c19785ce10adf
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-03 10:38:43 +0200

    day 4 - DOM form and inputs - ex. 1.2

--- 1_Zadania/Day 4/7_DOM_Inputy_i_formularze/js/zadanie02.js
+++ 1_Zadania/Day 4/7_DOM_Inputy_i_formularze/js/zadanie02.js
@@ -1,3 +1,36 @@
-/**
- * Created by Jacek on 2016-01-12.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var headerDiv = document.querySelector('.page-header');
+    var appleImg = headerDiv.children[1];
+    var ubuntuImg = headerDiv.children[2];
+    var windowsImg = headerDiv.children[3];
+    var select = document.querySelector('select');
+
+    appleImg.style.display = "none"
+    windowsImg.style.display = "none"
+    ubuntuImg.style.display = "none"
+
+    select.addEventListener('click', function() {
+        if (this.value === "Windows") {
+            appleImg.style.display = "none"
+            ubuntuImg.style.display = "none"
+            windowsImg.style.display = "block"
+        }
+        else if (this.value === "Os X") {
+            appleImg.style.display = "block"
+            ubuntuImg.style.display = "none"
+            windowsImg.style.display = "none"
+        }
+        else {
+            appleImg.style.display = "none"
+            ubuntuImg.style.display = "block"
+            windowsImg.style.display = "none"
+        }
+
+    });
+
+
+
+
+
+});
