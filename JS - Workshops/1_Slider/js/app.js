17e552582377d72d76ed429a312a79dd55410210
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-03 17:42:11 +0200

    JS workshops - slider

--- JS - Workshops/1_Slider/js/app.js
+++ JS - Workshops/1_Slider/js/app.js
@@ -1,3 +1,32 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var nextButton = document.getElementById('nextPicture');
+    var prevButton = document.getElementById('prevPicture');
+    var listElements = document.querySelectorAll('li');
+    var indexOfImg = 0;
+
+    listElements[indexOfImg].classList.add('visible');
+
+    nextButton.addEventListener('click', function(event) {
+        listElements[indexOfImg].classList.remove('visible');
+        indexOfImg++
+        if (indexOfImg >= listElements.length) {
+            indexOfImg = 0;
+        }
+        listElements[indexOfImg].classList.add('visible');
+    });
+
+    prevButton.addEventListener('click', function(event) {
+        listElements[indexOfImg].classList.remove('visible');
+        indexOfImg--
+        if (indexOfImg < 0) {
+            indexOfImg = listElements.length - 1;
+        }
+        listElements[indexOfImg].classList.add('visible');
+    });
+
+
+
+
+
+});
