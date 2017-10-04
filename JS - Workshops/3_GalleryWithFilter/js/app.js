ee5a3902fb76209d3a2ac04249d1fb8acea03898
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-04 14:44:03 +0200

    JS workshops - Gallery with filter

--- JS - Workshops/3_GalleryWithFilter/js/app.js
+++ JS - Workshops/3_GalleryWithFilter/js/app.js
@@ -1,3 +1,26 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener('DOMContentLoaded', function() {
+
+    var images = document.querySelectorAll('img');
+    var hideButton = document.querySelector('#hideButton');
+    var showButton = document.querySelector('#showButton');
+    var input = document.querySelector('#tagInput');
+    var inputText = "";
+
+    showButton.addEventListener('click', function(event) {
+        inputText = input.value
+        for (var i = 0; i < images.length; i++) {
+            if (images[i].dataset.tag.indexOf(inputText) != -1) {
+                images[i].classList.remove('invisible');
+            }
+        }
+    });
+
+    hideButton.addEventListener('click', function(event) {
+        for (var i = 0; i < images.length; i++) {
+            inputText = input.value
+            if (images[i].dataset.tag.indexOf(inputText) != -1) {
+                images[i].classList.add('invisible');
+            }
+        }
+    });
+});
