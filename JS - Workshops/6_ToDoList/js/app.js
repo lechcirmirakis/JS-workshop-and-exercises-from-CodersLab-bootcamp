d86e11cb18aadfed0e8bc728f2ab77941f134353
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-05 13:13:47 +0200

    JS workshops - ToDoList - 03 add tasks conditions

--- JS - Workshops/6_ToDoList/js/app.js
+++ JS - Workshops/6_ToDoList/js/app.js
@@ -4,49 +4,59 @@ document.addEventListener('DOMContentLoaded', function() {
     var buttonAddTask = document.getElementById('addTaskButton');
     var listTask = document.getElementById('taskList');
     var buttonRemoveFinishedTask = document.getElementById('removeFinishedTasksButton');
+    var body = document.querySelector('body');
+    var alert = document.querySelector('.alert');
 
     // event for Add Task Button
-    buttonAddTask.addEventListener('click', function() {
-        // create new elementos of ul
-        var newTask = document.createElement('li');
-        var newH2 = document.createElement('h2');
-        var newButtonDelete = document.createElement('button');
-        var newButtonComplete = document.createElement('button');
+    buttonAddTask.addEventListener('click', function(event) {
 
-        // event for Delete button
-        newButtonDelete.addEventListener('click', function() {
-            this.parentElement.parentElement.removeChild(newTask);
-        });
+        // First Condition for Task length value
+        if (inputTask.value.length >= 5 && inputTask.value.length <= 100) {
+            alert.innerText = "";
+            // create new elementos of ul
+            var newTask = document.createElement('li');
+            var newH2 = document.createElement('h2');
+            var newButtonDelete = document.createElement('button');
+            var newButtonComplete = document.createElement('button');
 
-        // event for Complete Button
-        newButtonComplete.addEventListener('click', function() {
-            this.previousElementSibling.previousElementSibling.classList.toggle('complete_h2');
-        });
+            // event for Delete button
+            newButtonDelete.addEventListener('click', function() {
+                this.parentElement.parentElement.removeChild(newTask);
+            });
 
-        newButtonComplete.innerText = "Complete"
-        newButtonComplete.classList.add('Complete_button');
+            // event for Complete Button
+            newButtonComplete.addEventListener('click', function() {
+                this.previousElementSibling.previousElementSibling.classList.toggle('complete_h2');
+            });
 
-        // adds the created elements
-        listTask.appendChild(newTask);
-        newTask.appendChild(newH2);
-        newTask.appendChild(newButtonDelete);
-        newTask.appendChild(newButtonComplete);
-        newH2.innerText = inputTask.value;
-        newButtonDelete.innerText = "Delete";
+            newButtonComplete.innerText = "Complete"
+            newButtonComplete.classList.add('Complete_button');
 
-        // event for Remove Finished Task button
-        removeFinishedTasksButton.addEventListener("click", function() {
-            var toDeleteTask = listTask.querySelectorAll('li');
-            for (var i = 0; i < toDeleteTask.length; i++) {
-                if (toDeleteTask[i].firstElementChild.className === "complete_h2") {
-                    toDeleteTask[i].parentElement.removeChild(toDeleteTask[i]);
+            // adds the created elements
+            listTask.appendChild(newTask);
+            newTask.appendChild(newH2);
+            newTask.appendChild(newButtonDelete);
+            newTask.appendChild(newButtonComplete);
+            newH2.innerText = inputTask.value;
+            newButtonDelete.innerText = "Delete";
+
+            // event for Remove Finished Task button
+            removeFinishedTasksButton.addEventListener("click", function() {
+                var toDeleteTask = listTask.querySelectorAll('li');
+                for (var i = 0; i < toDeleteTask.length; i++) {
+                    if (toDeleteTask[i].firstElementChild.className === "complete_h2") {
+                        toDeleteTask[i].parentElement.removeChild(toDeleteTask[i]);
+                    }
                 }
-            }
-        });
-
-
-
+            });
+        }
+        // First Condition for Task length value
+        else if (inputTask.value.length < 5) {
+            alert.innerText = "Your task is too short, it must have a minimum of 5 characters"
+        }
+        else {
+            alert.innerText = "Your task is too long, it must have a maximum of 100 characters"
+        }
     });
-
 
 });
