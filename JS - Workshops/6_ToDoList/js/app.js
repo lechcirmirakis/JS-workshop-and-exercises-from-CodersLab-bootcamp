c66390eeb85578bb4a6679b466e5f74b642f046c
GruzinHW <lech.cirmirakis@go2.pl>
2017-10-05 14:21:59 +0200

    JS workshops - ToDoList - 04 - add work counter

--- JS - Workshops/6_ToDoList/js/app.js
+++ JS - Workshops/6_ToDoList/js/app.js
@@ -4,14 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var buttonAddTask = document.getElementById('addTaskButton');
     var listTask = document.getElementById('taskList');
     var buttonRemoveFinishedTask = document.getElementById('removeFinishedTasksButton');
-    var body = document.querySelector('body');
     var alert = document.querySelector('.alert');
+
+    var counter = 0;
+    var countSpan = document.querySelector('.count>span');
+    countSpan.innerText = counter;
 
     // event for Add Task Button
     buttonAddTask.addEventListener('click', function(event) {
 
         // First Condition for Task length value
         if (inputTask.value.length >= 5 && inputTask.value.length <= 100) {
+            // Add Task Counter
+            counter++
+            countSpan.innerText = counter;
 
             // delete alert innerText
             alert.innerText = "";
@@ -24,12 +30,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // event for Delete button
             newButtonDelete.addEventListener('click', function() {
+                counter--
+                countSpan.innerText = counter;
                 this.parentElement.parentElement.removeChild(newTask);
             });
 
             // event for Complete Button
             newButtonComplete.addEventListener('click', function() {
                 this.previousElementSibling.previousElementSibling.classList.toggle('complete_h2');
+                if (this.previousElementSibling.previousElementSibling.classList.contains("complete_h2")) {
+                     counter--;
+                     countSpan.innerText = counter;
+
+                 } else {
+                     counter++;
+                     countSpan.innerText = counter;
+                 }
             });
 
             newButtonComplete.innerText = "Complete"
@@ -49,7 +65,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 var toDeleteTask = listTask.querySelectorAll('li');
                 for (var i = 0; i < toDeleteTask.length; i++) {
                     if (toDeleteTask[i].firstElementChild.className === "complete_h2") {
-                        toDeleteTask[i].parentElement.removeChild(toDeleteTask[i]);
+                        toDeleteTask[i].parentElement.removeChild(toDeleteTask[i]);    
                     }
                 }
             });
@@ -58,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function() {
         else if (inputTask.value.length < 5) {
             alert.innerText = "Your task is too short, it must have a minimum of 5 characters"
         }
-        
+
         else {
             alert.innerText = "Your task is too long, it must have a maximum of 100 characters"
         }
