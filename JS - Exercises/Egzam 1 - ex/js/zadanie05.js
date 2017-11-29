886f1669af9e076c15e8e65db7537c496e3800ad
GruzinHW <lech.cirmirakis@go2.pl>
2017-11-29 22:43:03 +0100

    egzam javascript 1 - zadanie 5

---
+++ JS - Exercises/Egzam 1 - ex/js/zadanie05.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var buttons = document.getElementsByTagName('button');
+  var div = document.getElementById('container');
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function() {
+
+      div.innerText = this.getAttribute('data-text');
+
+    });
+  }
+
+});
