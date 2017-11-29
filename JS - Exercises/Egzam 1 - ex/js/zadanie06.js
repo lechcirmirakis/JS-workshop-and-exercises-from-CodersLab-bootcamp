072fce7f16dfa9f95ee544f81ac2fe0335f673fc
GruzinHW <lech.cirmirakis@go2.pl>
2017-11-29 23:18:09 +0100

    egzam javascript 1 - zadanie 6

---
+++ JS - Exercises/Egzam 1 - ex/js/zadanie06.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var button_1 = document.querySelector('#button-1');
+  var button_2 = document.querySelector('#button-2');
+  var button_3 = document.querySelector('#button-3');
+  var list = document.querySelector('#shopping-list');
+
+
+  button_1.addEventListener('click', function() {
+    var newLi = document.createElement('li');
+    newLi.innerText = "Chleb";
+    list.appendChild(newLi);
+  });
+
+  button_2.addEventListener('click', function() {
+    var lastChild = list.lastElementChild;
+    lastChild.parentElement.removeChild(lastChild);
+  });
+
+  button_3.addEventListener('click', function() {
+    var secondLi = list.children[1];
+    if (list.children.length > 2) {
+      var newLi = secondLi.cloneNode(true);
+      list.appendChild(newLi);
+    } else {
+      console.log("niema czego kopiowac :)");
+    }
+  });
+
+});
