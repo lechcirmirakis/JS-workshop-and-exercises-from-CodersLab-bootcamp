62e7c5ecde65b0aa4041ceac640df353997887cd
GruzinHW <lech.cirmirakis@go2.pl>
2017-11-29 22:14:20 +0100

    egzam javascript 1 - zadanie 4 - 4

--- JS - Exercises/Egzam 1 - ex/js/zadanie04.js
+++ JS - Exercises/Egzam 1 - ex/js/zadanie04.js
@@ -5,22 +5,51 @@ var sample = document.querySelectorAll('.sample_class');
 function getTagsOfElements(element) {
   var tab = [];
   for (var i = 0; i < element.length; i++) {
-    tab.push(element[i].tagName)
+    tab.push(element[i].tagName);
   }
   return tab
 }
 console.log(getTagsOfElements(sample));
 
-// 2 Szukanie nazw class
+// 2 Szukanie nazw klas
+
+var sampleID = document.querySelector('#sample_id');
+
+function getClassesOfElement(element) {
+  var tab = [];
+  tab = element.className;
+  return tab
+}
+console.log(getClassesOfElement(sampleID));
+
+// 3 Szukanie tekstu
 
 var listElements = document.querySelectorAll('.sample_class_2 li')
 
 function getInnerTextsOfElements(element) {
   var tab = [];
   for (var i = 0; i < element.length; i++) {
-    tab.push(element[i].innerText)
+    tab.push(element[i].innerText);
   }
   return tab
 }
 
 console.log(getInnerTextsOfElements(listElements));
+
+// 4 Szukanie adresow linków
+
+var links = document.querySelectorAll('a');
+
+function getAddressesOfElements(element) {
+  var tab = [];
+  for (var i = 0; i < element.length; i++) {
+    if (element[i].hasAttribute('href') != 0) {
+      tab.push(element[i].getAttribute('href'));
+    }
+  }
+  return tab
+}
+console.log(getAddressesOfElements(links));
+
+
+// 5 Szukanie tagów dzieci
